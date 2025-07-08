7b1e0c1b604cf4def92d939d63c98def73f3ccbd
UJJAWAL JADAVPUR <ujjawalkjadavpur@gmail.com>
2025-07-08 13:25:11 +0530

    Initial Setup

---
+++ src/main/java/com/example/notestackapi/NoteStackApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.notestackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteStackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteStackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notestackapi/NoteStackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notestackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteStackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
